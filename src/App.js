23c9fab2931e0c3e0ea4d4d34b86de1270da2d2d
Michael Chop <007micle007@gmail.com>
2022-11-12 05:59:01 +0300

    Added a Modal project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Modal from "./projects/Modal/Modal";
+
+function App() {
+  return (
+    <div>
+      <Modal />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/projects/Modal/Modal.js
@@ -0,0 +1,25 @@
+import React, {useState} from 'react';
+import './modal.scss'
+
+function Modal() {
+    const [open, setOpen] = useState(false)
+
+    return (
+        <div className="App">
+            <button onClick={() => setOpen(true)} className="open-modal-btn">✨ Открыть окно</button>
+
+            <div className={`overlay animated ${open ? 'show' : ''}`}>
+                <div className="modal">
+                    <svg height="200" viewBox="0 0 200 200" width="200" onClick={() => setOpen(false)}>
+                        <title/>
+                        <path
+                            d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z"/>
+                    </svg>
+                    <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif"/>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Modal;
